ff52c12c124c3fa7d4bdeba520fcf8e6d6fe3485
Xushnud Egamberdiyev <140577111+Xushnud-Egamberdiyev@users.noreply.github.com>
2024-04-21 18:21:56 +0500

    Add Global Exception handler

--- Todo/Todo.API/Program.cs
+++ Todo/Todo.API/Program.cs
@@ -1,4 +1,5 @@
 
+using Todo.API.Middlewares;
 using Todo.Domain.Entities.Auth;
 using Todo.Infrsatructure;
 using Todo.Infrsatructure.Persistance;
@@ -47,6 +48,7 @@ namespace Todo.API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
 
             app.UseHttpsRedirection();
 
---
+++ Todo/Todo.Application/Exceptions/CustomException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Application.Exceptions
+{
+    public class CustomException : Exception
+    {
+        public int StatuCode { get; set; }
+        public CustomException(int code, string message) : base(message)
+        {
+            StatuCode = code;
+        }
+    }
+}
