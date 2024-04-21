9ca3b462716de63e3422a5435d5ba1d0989ad4d6
Tursunboev-Hayotilla <coder0227@gmail.com>
2024-04-21 16:10:12 +0500

    CreateCommentHandler/Command done

---
+++ Todo/Todo.Application/UseCases/Commands/CreateCommentCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Entities.Auth;
+using Todo.Domain.Entities;
+using MediatR;
+
+namespace Todo.Application.UseCases.Commands
+{
+    public class CreateCommentCommand:IRequest<Comment>
+    {
+        public string Message { get; set; }
+        public Guid SenderId { get; set; }
+        public Guid IssueId { get; set; }
+        public virtual User Sender { get; set; }
+        public virtual Issue Issue { get; set; }
+    }
+}
