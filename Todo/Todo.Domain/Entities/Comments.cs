67a10560884efae62c67dac68c0e4158f9834bae
ozodbek <olimjanovozodbek32@gmail.com>
2024-04-21 10:07:40 +0500

    .

--- Todo/Todo.Domain/Entities/Comments.cs
+++ Todo/Todo.Domain/Entities/Comments.cs
@@ -11,6 +11,8 @@ namespace Todo.Domain.Entities
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Message { get; set; }
         public Guid SenderId {  get; set; }
-        public Guid RecieverId {  get; set; }
+        public Guid IssueId {  get; set; }
+        public virtual User Sender { get; set; }
+        public virtual Issue Issue { get; set; }
     }
 }
--- Todo/Todo.Domain/Entities/Issue.cs
+++ Todo/Todo.Domain/Entities/Issue.cs
@@ -13,10 +13,11 @@ namespace Todo.Domain.Entities
         public string FullName { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public Guid ProgrammerId { get; set; }
         public IssueStatus Status {  get; set; } 
-        public List<Comments> Comments {  get; set; }
+        public virtual List<Comments> Comments {  get; set; }
         public DateTimeOffset CreatedDate {  get; set; } = DateTime.UtcNow;
         public DateTimeOffset Deadline { get; set;}
+        public Guid ProgrammerId { get; set; }
+        public virtual User Programmer {  get; set; }
     }
 }
